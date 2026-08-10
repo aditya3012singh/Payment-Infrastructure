cea5181ec53899ad110a58741450b31f0111eff6
Aditya <aditya.2428cseai@gmail.com>
2026-08-10 13:01:42 +0530

    feat: added payment repository

---
+++ src/main/java/com/payment/payflow/repository/PaymentRepository.java
@@ -0,0 +1,16 @@
+package com.payment.payflow.repository;
+
+import com.payment.payflow.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+    
+    // Spring Data JPA will automatically implement this method based on the name!
+    // We will use this to quickly check if a payment with this key already exists.
+    Optional<Payment> findByIdempotencyKey(String idempotencyKey);
+}
